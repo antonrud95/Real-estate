f36f20b1e0c170931534819231cee313cc629549
arud95 <antonrud95@yandex.ru>
2020-11-21 13:21:41 +0200

    wip: fix login styles

--- frontend/src/components/sections/login/login-section.component.tsx
+++ frontend/src/components/sections/login/login-section.component.tsx
@@ -1,65 +1,19 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React from 'react'
 import styles from './login-section.module.scss'
 
-import { UserContext } from '~/contexts/user-context.context'
-
-const LoginSection = ({ history }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-
-  const { user, setUser } = useContext(UserContext)
-  // console.log('user', user)
-
-  useEffect(() => {
-    if (user) {
-      history.push('/')
-    }
-  }, [user])
-
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-
-    try {
-      const response = await fetch('http://localhost:1337/auth/local/', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          identifier: email,
-          password,
-        }),
-      })
-
-      const data = await response.json()
-      console.log('data', data)
-
-      if (data.message) {
-        setError(data.message[0].messages[0].message)
-        return
-      }
-      setUser(data)
-    } catch (err) {
-      setError('Something went wrong' + err)
-    }
-  }
-
-  const emailHandler = (event) => {
-    setError('')
-    setEmail(event.target.value)
-  }
-
-  const passwordHandler = (event) => {
-    setError('')
-    setPassword(event.target.value)
-  }
-
+const LoginSection = ({
+  onSubmit,
+  onChangeEmail,
+  onChangePassword,
+  email,
+  password,
+  error,
+}) => {
   return (
     <div className={styles.loginSection}>
-      <form onSubmit={handleSubmit} className={styles.formStyles}>
-        <input type="email" value={email} onChange={emailHandler} />
-        <input type="password" value={password} onChange={passwordHandler} />
+      <form onSubmit={onSubmit} className={styles.formStyles}>
+        <input type="email" value={email} onChange={onChangeEmail} />
+        <input type="password" value={password} onChange={onChangePassword} />
         <button>Login</button>
         {error && <p>{error}</p>}
       </form>
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,16 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { navigate } from 'gatsby'
 
 import Layout from '~/components/layout'
 import SEO from '~/components/seo.component'
 import LoginSection from '~/components/sections/login/login-section.component'
 
+import { UserContext } from '~/contexts/user-context.context'
+
 const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const { user, setUser } = useContext(UserContext)
+  // console.log('user', user)
+
+  useEffect(() => {
+    if (user) {
+      navigate('/hotels')
+    }
+  }, [user])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await fetch('http://localhost:1337/auth/local/', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          identifier: email,
+          password,
+        }),
+      })
+
+      const data = await response.json()
+      console.log('data', data)
+
+      if (data.message) {
+        setError(data.message[0].messages[0].message)
+        return
+      }
+      setUser(data)
+    } catch (err) {
+      setError('Something went wrong' + err)
+    }
+  }
+
+  const emailHandler = (event) => {
+    setError('')
+    setEmail(event.target.value)
+  }
+
+  const passwordHandler = (event) => {
+    setError('')
+    setPassword(event.target.value)
+  }
+
   return (
     <Layout>
       <SEO title="Unikorns Starter Kit" />
-      <LoginSection />
+      <LoginSection
+        onSubmit={handleSubmit}
+        onChangeEmail={emailHandler}
+        onChangePassword={passwordHandler}
+        error={error}
+        password={password}
+        email={email}
+      />
     </Layout>
   )
 }
